e5a49ee492aaab3f7872876271ef22d3693b22b4
Lukas Larsed <git@lukaslarsed.com>
2015-01-03 00:37:38 +0100

    Values components added

--- src/lib/Const.java
+++ src/lib/Const.java
@@ -5,4 +5,10 @@ public class Const {
 	//Control panel
 	public final static String CONTROL_PANEL = "Control panel";
 	public final static String ANIM_SPEED = "Animation speed";
+	public final static String CONT_VALUES = "Values";
+	public final static String LAB_LOW = "Min";
+	public final static String LAB_HIGH = "Max";
+	public final static String LAB_COUNT = "Count";
+	public final static int VALUES_LNGT = 10;
+	
 }
--- src/view/ControlPanel.java
+++ src/view/ControlPanel.java
@@ -12,7 +12,7 @@ import lib.Const;
 
 public class ControlPanel extends JPanel {
 
-	private JPanel algPanel, valuesPanel, speedPanel;
+	private JPanel algPanel, labelValuesPanel, valuesPanel, valuesWrapperPanel, speedPanel;
 	private JLabel bubblesortLabel, lowestValueLabel, highestValueLabel,
 			countLabel;
 	private JSlider speed;
@@ -20,8 +20,25 @@ public class ControlPanel extends JPanel {
 
 	public ControlPanel() {
 		this.setLayout(new BorderLayout());
-
 		
+		//Values and labels
+		lowestValueLabel = new JLabel(Const.LAB_LOW);
+		highestValueLabel = new JLabel(Const.LAB_HIGH);
+		countLabel = new JLabel(Const.LAB_COUNT);
+		labelValuesPanel = new JPanel(new BorderLayout());
+		labelValuesPanel.add(lowestValueLabel, BorderLayout.NORTH);
+		labelValuesPanel.add(highestValueLabel, BorderLayout.CENTER);
+		labelValuesPanel.add(countLabel, BorderLayout.SOUTH);
+		min = new JTextField(Const.VALUES_LNGT);
+		max = new JTextField(Const.VALUES_LNGT);
+		count = new JTextField(Const.VALUES_LNGT);
+		valuesPanel = new JPanel(new BorderLayout());
+		valuesPanel.add(min, BorderLayout.NORTH);
+		valuesPanel.add(max, BorderLayout.CENTER);
+		valuesPanel.add(count, BorderLayout.SOUTH);
+		valuesWrapperPanel = new JPanel(new BorderLayout());
+		valuesWrapperPanel.add(labelValuesPanel, BorderLayout.WEST);
+		valuesWrapperPanel.add(valuesPanel, BorderLayout.EAST);
 		
 		//Speedslider
 		speed = new JSlider(JSlider.HORIZONTAL, 10, 30, 12);
@@ -36,8 +53,11 @@ public class ControlPanel extends JPanel {
 		//Borders
 		this.setBorder(BorderFactory.createTitledBorder(Const.CONTROL_PANEL));
 		speedPanel.setBorder(BorderFactory.createTitledBorder(Const.ANIM_SPEED));
-
-		this.add(speedPanel, BorderLayout.CENTER);
+		valuesWrapperPanel.setBorder(BorderFactory.createTitledBorder(Const.CONT_VALUES));
+		
+		//Adding all panels
+		this.add(valuesWrapperPanel, BorderLayout.WEST);
+		this.add(speedPanel, BorderLayout.EAST);
 
 	}
 
